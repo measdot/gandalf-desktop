3f028e3544adbdcc7b7810564ee69cfb92787b0c
Kumar Gaurav <kumar@Kumars-MBP.Dlink>
2018-07-02 12:22:51 +0530

    jira client integrated

--- src/components/home.js
+++ src/components/home.js
@@ -7,14 +7,15 @@ const preferences = require('./preferences')
 
 const tabsTemplate = pug.compileFile('src/templates/tabs.pug');
 const preferencesTemplate = pug.compileFile('src/templates/preferences.pug');
+var mainLayoutStyle = 'border:1px solid grey';
 const config = {
 	name: 'layout-home',
 	padding: 1,
 	panels: [
-		{ type: 'left', size: 300, minSize:250, resizable: true},
-		{ type: 'main', content: tabsTemplate()},
-		{ type: 'right', size: 350, minSize:350, resizable: true, content:preferencesTemplate()},
-		{ type: 'bottom', size: 35, resizable: false, toolbar: mainToolbar}
+		{ type: 'left', size: 300, minSize:250, style:mainLayoutStyle, resizable: true},
+		{ type: 'main', content: tabsTemplate(), style:mainLayoutStyle},
+		{ type: 'right', size: 350, minSize:350, style:mainLayoutStyle, resizable: true, content:preferencesTemplate()},
+		{ type: 'bottom', size: 35, resizable: false, style:mainLayoutStyle, toolbar: mainToolbar}
 	]
 };
 
@@ -29,3 +30,23 @@ dashboard.init();
 
 // init right panel content
 preferences.init();
+
+// With ES5
+var JiraApi = require('jira-client');
+
+// Initialize
+var jira = new JiraApi({
+	protocol: 'https',
+	host: 'pipingrock.atlassian.net',
+	username: 'kgaurav@pipingrock.com',
+	password: '',
+	apiVersion: '2'
+});
+jira.findIssue("ECI-38")
+	.then(function(issue) {
+		w2ui['layoutdashboard'].content('main', '<div><p>Jira Issue Number: '+issue.key+'</p><p>Jira Issue Summary: '+issue.fields.summary+'</p><p>Jira Issue Status: '+issue.fields.status.name+'</p></div>');
+		console.log(issue);
+	})
+	.catch(function(err) {
+		console.error(err);
+	});
